a5d2ea0b1cfd9a0bd69a63438ccc4f7ccab1c910
mitsuTanak <130664643+mitsuTanak@users.noreply.github.com>
2023-11-24 12:46:41 -0300

    ,

--- produtos/listar.php
+++ produtos/listar.php
@@ -14,6 +14,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../style.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <title>Produtos</title>
 </head>
 <body>
@@ -25,7 +30,7 @@
 
         <h2>Lendo e carregando todos os produtos</h2>
 
-        <p><a href="inserir.php" style="color:blue;">Inserir um novo Produto</a></p>
+        <p><a href="inserir.php" style="color:blue;"><button type="button" class="btn btn-primary">Inserir um novo Produto</button></a></p>
 
         <hr>
 
@@ -59,8 +64,8 @@
 
                     <!-- Link dinâmico -->
                     <p>
-                        <a href="atualizar.php?id=<?=$produto['id']?>" style="color:blue;">Atualizar</a>
-                        <a class="excluir" href="excluir.php?id=<?=$produto['fabricante']?>" style="color:red;">excluir</a>
+                        <a href="atualizar.php?id=<?=$produto['id']?>" style="color:blue;"><button type="button" class="btn btn-success">Atualizar</button></a>
+                        <a class="excluir" href="excluir.php?id=<?=$produto['fabricante']?>" style="color:red;"><button type="button" class="btn btn-danger">excluir</button></a>
                     </p>
 
                     <hr>
